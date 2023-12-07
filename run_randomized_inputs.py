0f01b25c7b4d8e5bfcd509eaffb35e72a04386e5
Bendegúz Györök <119816494+gyorokbendeguz@users.noreply.github.com>
2023-12-07 11:39:25 +0100

    Added multiple simulation option

--- run_randomized_inputs.py
+++ run_randomized_inputs.py
@@ -21,11 +21,6 @@ def write_single(results, filename):  # writer in case of Single Input or Single
     writer = csv.writer(myfile)
     for result in results:
       writer.writerow([result])
-
-# Datafolder structure
-now = datetime.now()
-strNow = now.strftime("%Y_%m_%d_%H%M%S")
-strFolder = "SimData/Randomized_inputs_" + strNow
    
 
 simulator = sp.SimulatorInterface()
@@ -38,7 +33,6 @@ init_options = ['panSim', '-r', ' ', '--diags', '0', '--quarantinePolicy', '0', 
                 '--diseaseProgressionScaling', '0.94,1.03,0.813,0.72,0.57,0.463,0.45',
                 '--closures','inputConfigFiles/emptyRules.json'
                 ]
-simulator.initSimulation(init_options)
 
 input_sets = [["TPdef", "PLNONE", "CFNONE", "SONONE", "QU0", "MA1.0"], ["TPdef", "PL0", "CFNONE", "SONONE", "QU0", "MA1.0"], ["TPdef", "PLNONE", "CF2000-0500", "SONONE", "QU0", "MA1.0"],
               ["TPdef", "PLNONE", "CFNONE", "SO12", "QU0", "MA1.0"], ["TPdef", "PLNONE", "CFNONE", "SO3", "QU0", "MA1.0"], ["TPdef", "PLNONE", "CFNONE", "SONONE", "QU1", "MA1.0"],
@@ -47,39 +41,50 @@ input_sets = [["TPdef", "PLNONE", "CFNONE", "SONONE", "QU0", "MA1.0"], ["TPdef",
               ["TP015", "PLNONE", "CFNONE", "SO12", "QU2", "MA1.0"], ["TP015", "PLNONE", "CFNONE", "SO3", "QU2", "MA1.0"], ["TP015", "PLNONE", "CFNONE", "SONONE", "QU1", "MA0.8"],
               ["TP035", "PLNONE", "CFNONE", "SONONE", "QU2", "MA0.8"], ["TP035", "PL0", "CFNONE", "SO3", "QU2", "MA0.8"], ["TP035", "PLNONE", "CF2000-0500", "SO3", "QU2", "MA0.8"]]
 
-# initial run options
-run_options = input_sets[0]
-input_idx = 0
+nSimulations = 35
 
-CONSTANT_PERIOD = 7  # run every input for 7 days
-ENDTIME = 250  # days
+for idxRun in range(nSimulations):
+    # initialization
+    simulator.initSimulation(init_options)
+    run_options = input_sets[0]
+    input_idx = 0
 
-results_agg = []
-inputs_agg = []
-day_counter = 0
-for i in range(0, ENDTIME):
-    results = simulator.runForDay(run_options)
-    day_counter += 1
-    if day_counter == CONSTANT_PERIOD:
-       input_idx, run_options = get_rnd_runoptions(input_sets)
-       day_counter = 0
-    results_agg.append(results)
-    inputs_agg.append(input_idx)
+    CONSTANT_PERIOD = 7  # run every input for 7 days
+    ENDTIME = 250  # days
+
+    # Datafolder structure
+    now = datetime.now()
+    strNow = now.strftime("%Y_%m_%d_%H%M%S")
+    strFolder = "SimData/Randomized_inputs_" + strNow
+
+    results_agg = []
+    inputs_agg = []
+    day_counter = 0
+    for i in range(0, ENDTIME):
+        results = simulator.runForDay(run_options)
+        day_counter += 1
+        if day_counter == CONSTANT_PERIOD:
+           input_idx, run_options = get_rnd_runoptions(input_sets)
+           day_counter = 0
+        results_agg.append(results)
+        inputs_agg.append(input_idx)
 
 
-print("Simulation finished...")
+    print(f"Simulation number {idxRun} finished...")
 
-nHospitalized = get_results(results_agg)
+    nHospitalized = get_results(results_agg)
 
-# Saving the results
-currentDir = os.getcwd()
-#path = os.path.join(currentDir, )
-os.mkdir(strFolder)
+    # Saving the results
+    currentDir = os.getcwd()
+    #path = os.path.join(currentDir, )
+    os.mkdir(strFolder)
 
-strInputPath = os.path.join(strFolder, "input.csv")
-strOutputPath = os.path.join(strFolder, "output.csv")
+    strInputPath = os.path.join(strFolder, "input.csv")
+    strOutputPath = os.path.join(strFolder, "output.csv")
 
-write_single(inputs_agg, strInputPath)
-write_single(nHospitalized, strOutputPath)
+    write_single(inputs_agg, strInputPath)
+    write_single(nHospitalized, strOutputPath)
 
-print(f"Results saved into {strFolder} directory.")
+    print(f"Results saved into {strFolder} directory.")
+
+print(f"--------------- {nSimulations} number of simulations finished ---------------")
